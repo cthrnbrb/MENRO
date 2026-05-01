6736def8393a16f8657141b8b87142184ecb4b46
cthrnbrb <catherinebarba2005@gmail.com>
2026-05-01 18:27:11 +0800

    Backend First and Final

--- backend/database/migrations/2026_04_20_000014_create_certificates_table.php
+++ backend/database/migrations/2026_04_20_000014_create_certificates_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
             $table->integer('id', false, true)->primary()->autoIncrement();
             $table->integer('user_id', false, true)->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->integer('partner_user_id', false, true)->nullable();
+            $table->foreign('partner_user_id')->references('id')->on('users')->onDelete('set null');
             $table->integer('organization_id', false, true)->nullable();
             $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('set null');
             $table->string('certificate_number', 50)->unique();
